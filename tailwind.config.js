0f6529a1b280d7e1b5c9f52a0fda001726b3e7ec
muhammadluttfy <muhammadlutfi.storage@gmail.com>
2022-02-16 18:21:38 +0700

    3 - 9 | Instalasi & Konfigurasi, Basic Utility, Pseudo Class & Dark Mode, Transform, transition, Animation, Layouting and Responsive Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
     extend: {},
+    fontFamily: {
+      poppins: ["Poppins"],
+    },
+    // colors: {
+    //   "regal-blue": "#243c5a",
+    // },
   },
   plugins: [],
 };
